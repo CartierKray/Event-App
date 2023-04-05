5ce27eb0a9917a3c1484a7253baab2f72a2a0358
Krishan Ramdin <Krishanramdin_@hotmail.com>
2023-04-05 12:29:07 +0200

    Changes

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -1,6 +1,7 @@
-import { Outlet } from 'react-router-dom';
-import { Navigation } from './Navigation';
-import { Box } from '@chakra-ui/react';
+import { Outlet } from "react-router-dom";
+import { Navigation } from "./Navigation";
+import { Box } from "@chakra-ui/react";
+import React from "react";
 
 export const Root = () => {
   return (
--- src/pages/EventsPage.jsx
+++ src/pages/EventsPage.jsx
@@ -1,4 +1,5 @@
 import { Heading } from "@chakra-ui/react";
+import React from "react";
 
 export const EventsPage = () => {
   return <Heading>List of events</Heading>;
