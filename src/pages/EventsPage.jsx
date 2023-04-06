d01ac173b6f683b302fcf54367a03708b14f6718
Krishan Ramdin <Krishanramdin_@hotmail.com>
2023-04-06 11:53:39 +0200

    Changes Made

--- src/pages/EventsPage.jsx
+++ src/pages/EventsPage.jsx
@@ -1,6 +1,20 @@
 import { Heading } from "@chakra-ui/react";
 import React from "react";
+import { useLoaderData } from "react-router-dom";
+
+const loader = async () => {
+  const events = await fetch("http://localhost:3000/events");
+  const categories = await fetch("http://localhost:3000/categories");
+
+  return { events: await events.json(), categories: await categories.json() };
+};
 
 export const EventsPage = () => {
-  return <Heading>List of events</Heading>;
+  const { events, categories } = useLoaderData();
+
+  return (
+    <>
+      <Heading>List of events</Heading>
+    </>
+  );
 };
