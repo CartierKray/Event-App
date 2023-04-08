c63e845dff7d6ee7cb8ddd0c8b4895633d8e742c
Krishan Ramdin <Krishanramdin_@hotmail.com>
2023-04-08 22:39:48 +0200

    Changed Nav font family

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,16 +1,24 @@
 import { Link } from "react-router-dom";
 import React from "react";
-import { Box, Flex, List } from "@chakra-ui/react";
+import { Box, Flex, List, Text } from "@chakra-ui/react";
 
 export const Navigation = () => {
   return (
     <Box as="nav" bg="bg-surface" boxShadow="xl" h={"40px"}>
       <Flex margin={"15px"} justifyContent={"space-evenly"}>
         <List>
-          <Link to="/">Events</Link>
+          <Link to="/">
+            <Text fontFamily={"Poppins"} fontSize={"20px"} fontWeight={"bold"}>
+              Events
+            </Text>
+          </Link>
         </List>
         <List>
-          <Link to="/event/1">Event</Link>
+          <Link to="/event/1">
+            <Text fontFamily={"Poppins"} fontSize={"20px"} fontWeight={"bold"}>
+              Event
+            </Text>
+          </Link>
         </List>
       </Flex>
     </Box>
