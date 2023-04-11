d3c83b17b6a1fef4d38131c48ffea704b0e0eae0
Krishan Ramdin <Krishanramdin_@hotmail.com>
2023-04-11 15:49:01 +0200

    Modify code

--- src/pages/EventsPage.jsx
+++ src/pages/EventsPage.jsx
@@ -16,20 +16,71 @@ import { AiFillTags } from "react-icons/ai";
 import { AddEvent } from "./AddEvent";
 import { useState } from "react";
 
-export const loader = async () => {
-  const events = await fetch("http://localhost:3000/events");
-  const categories = await fetch("http://localhost:3000/categories");
+const loader = async () => {
+  const [eventsResponse, categoriesResponse] = await Promise.all([
+    fetch("http://localhost:3000/events"),
+    fetch("http://localhost:3000/categories"),
+  ]);
 
-  return { events: await events.json(), categories: await categories.json() };
+  const [events, categories] = await Promise.all([
+    eventsResponse.json(),
+    categoriesResponse.json(),
+  ]);
+
+  return { events, categories };
 };
 
-export const EventsPage = () => {
+const EventListItem = ({ event, categories }) => {
+  return (
+    <List key={event.id} borderBottomWidth="1px" borderColor="gray.200" pb="6">
+      <Image src={event.image} alt={event.title} mb="4" borderRadius="md" />
+      <Link to={`/event/${event.id}`}>
+        <Heading as="h2" size="md">
+          {event.title}
+        </Heading>
+      </Link>
+      <Text color="gray.500" fontSize="sm" mb="3" mt="1">
+        Location: {event.location}
+      </Text>
+      <Text>{event.description}</Text>
+      <Text color="gray.500" fontSize="sm" mt="3">
+        Starts at: {new Date(event.startTime).toLocaleString()}
+      </Text>
+      <Text color="gray.500" fontSize="sm" mt="1">
+        End at: {new Date(event.endTime).toLocaleString()}
+      </Text>
+      <Flex gap={2} color="gray.500" fontSize="sm" mt="3">
+        Category:
+        <Flex gap={2}>
+          {categories
+            .filter((category) => event.categoryIds.includes(category.id))
+            .map((category) => (
+              <Box key={category.id}>
+                <Tag color="teal.700" fontSize="sm" bg="teal.200">
+                  <TagLeftIcon as={AiFillTags}></TagLeftIcon>
+                  <Text textTransform={"uppercase"} fontSize={"10"}>
+                    {category.name}
+                  </Text>
+                </Tag>
+              </Box>
+            ))}
+        </Flex>
+      </Flex>
+    </List>
+  );
+};
+
+const EventsPage = () => {
   const { events, categories } = useLoaderData();
-  const { search, setSearch } = useState("");
+  const [search, setSearch] = useState("");
 
   const handleChange = (event) => {
     setSearch(event.target.value);
   };
+
+  const filteredEvents = events.filter((event) =>
+    event.title.toLowerCase().includes(search.toLowerCase())
+  );
 
   return (
     <Box p="6">
@@ -45,60 +96,15 @@ export const EventsPage = () => {
           ></Input>
         </Box>
       </Center>
-
       <List spacing="6">
-        {events.map((event) => (
-          <List
-            key={event.id}
-            borderBottomWidth="1px"
-            borderColor="gray.200"
-            pb="6"
-          >
-            <Image
-              src={event.image}
-              alt={event.title}
-              mb="4"
-              borderRadius="md"
-            />
-            <Link to={`/event/${event.id}`}>
-              <Heading as="h2" size="md">
-                {event.title}
-              </Heading>
-            </Link>
-            <Text color="gray.500" fontSize="sm" mb="3" mt="1">
-              Location: {event.location}
-            </Text>
-            <Text>{event.description}</Text>
-            <Text color="gray.500" fontSize="sm" mt="3">
-              Starts at: {new Date(event.startTime).toLocaleString()}
-            </Text>
-            <Text color="gray.500" fontSize="sm" mt="1">
-              End at: {new Date(event.endTime).toLocaleString()}
-            </Text>
-            <Flex gap={2} color="gray.500" fontSize="sm" mt="3">
-              Category:
-              <Flex gap={2}>
-                {categories
-                  .filter((category) => event.categoryIds.includes(category.id))
-                  .map((category) => (
-                    <Box key={category.id}>
-                      <Tag color="teal.700" fontSize="sm" bg="teal.200">
-                        <TagLeftIcon as={AiFillTags}></TagLeftIcon>
-                        <Text textTransform={"uppercase"} fontSize={"10"}>
-                          {category.name}
-                        </Text>
-                      </Tag>
-                    </Box>
-                  ))}
-              </Flex>
-            </Flex>
-          </List>
+        {filteredEvents.map((event) => (
+          <EventListItem key={event.id} event={event} categories={categories} />
         ))}
-
         <AddEvent />
-
         <ListItem borderBottomWidth="1px" borderColor="gray.200"></ListItem>
       </List>
     </Box>
   );
 };
+
+export { loader, EventsPage };
