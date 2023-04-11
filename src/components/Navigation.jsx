2e48a4997645e83fa874b11b492d43dffa278f17
Krishan Ramdin <Krishanramdin_@hotmail.com>
2023-04-11 15:53:11 +0200

    Modified

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import React from "react";
-import { Box, Flex, List, Text } from "@chakra-ui/react";
+import { Box, Flex, List, ListItem, Text } from "@chakra-ui/react";
 
 export const Navigation = () => {
   return (
@@ -8,16 +8,28 @@ export const Navigation = () => {
       <Flex margin={"15px"} justifyContent={"space-evenly"}>
         <List>
           <Link to="/">
-            <Text fontFamily={"Poppins"} fontSize={"20px"} fontWeight={"bold"}>
+            <Text
+              fontFamily={"Poppins"}
+              fontSize={"20px"}
+              fontWeight={"bold"}
+              _hover={{ color: "teal" }}
+            >
               Events
             </Text>
+            <ListItem borderBottomWidth="1px" borderColor="gray.200"></ListItem>
           </Link>
         </List>
         <List>
           <Link to="/event/1">
-            <Text fontFamily={"Poppins"} fontSize={"20px"} fontWeight={"bold"}>
+            <Text
+              fontFamily={"Poppins"}
+              fontSize={"20px"}
+              fontWeight={"bold"}
+              _hover={{ color: "teal" }}
+            >
               Event
             </Text>
+            <ListItem borderBottomWidth="1px" borderColor="gray.200"></ListItem>
           </Link>
         </List>
       </Flex>
