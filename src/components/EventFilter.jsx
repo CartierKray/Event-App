09ab40412477c13231e4c16d9c5c7a238cde6d61
Krishan Ramdin <Krishanramdin_@hotmail.com>
2023-04-12 00:14:12 +0200

    Modify

--- src/components/EventFilter.jsx
+++ src/components/EventFilter.jsx
@@ -4,7 +4,7 @@ export const EventFilter = ({ value, onChange }) => {
   return (
     <Center>
       <RadioGroup
-        bgGradient="linear(to-r, rgba(26, 32, 44, 0.7), rgba(26, 32, 44, 0.90))"
+        bgGradient="linear(to-r, rgba(16, 28, 37, 0.6), rgba(12, 19, 28, 0.90))"
         boxShadow="0 0 10px rgba(0, 0, 0, 0.3)"
         borderColor={"gray.400"}
         p={4}
